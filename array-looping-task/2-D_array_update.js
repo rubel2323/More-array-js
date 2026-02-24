3e9740531582c3f8d64b3922526c41e4f77c29e6
Rubel <mr.adcadre41@gmail.com>
2026-02-24 11:04:03 +0600

    2-d array updated

---
+++ array-looping-task/2-D_array_update.js
@@ -0,0 +1,8 @@
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+const arr2d = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+arr2d[1][0] = 99;
+console.log(arr2d);
