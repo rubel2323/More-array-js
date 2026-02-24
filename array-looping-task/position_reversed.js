b58cb1825f017860deefb1fff995e8fb0b7fcdf2
Rubel <mr.adcadre41@gmail.com>
2026-02-24 09:46:25 +0600

    only positon of string reversed

---
+++ array-looping-task/position_reversed.js
@@ -0,0 +1,8 @@
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+const statement = "I am a hard working person";
+// let nameSplit = statement.split(" ");
+// let nameRev = nameSplit.reverse();
+// let nameJoin = nameRev.join(" ");
+// console.log(nameJoin);
+console.log(statement.split(" ").reverse().join(" "));
